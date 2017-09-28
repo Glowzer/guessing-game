3d0c85a9f869498146e1a9be2af53f879afafe1c
Glowzer <31700869+Glowzer@users.noreply.github.com>
2017-09-28 17:49:36 +0300

    Final version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this._max = 0;
+      this._min = 0;
+    }
 
     setRange(min, max) {
-
+      this._max = max;
+      this._min = min;
     }
 
     guess() {
-
+      return Math.ceil((this._max + this._min) / 2);
     }
 
     lower() {
-
+      this._max = Math.ceil((this._max + this._min) / 2);
     }
 
     greater() {
-
+      this._min = Math.ceil((this._max + this._min) / 2);
     }
 }
 
